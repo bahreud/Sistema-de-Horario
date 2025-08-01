bb0f9b7a6dd9db99c353486660bf6c4cf67b7720
bahreud <fsilvagomes2609@gmail.com>
2025-08-01 14:03:48 -0300

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,45 @@
+contas = []
+while True:
+    def login():
+        valida = [(input("Insira seu ID")), input("Insira sua senha: ")]
+        for i in contas:
+            if i == valida:
+                return True
+
+
+    def registro():
+        conta = [(input("Escolha seu ID")), input("Escolha sua senha: ")]
+        contas.append(conta)
+        print("Conta registrada!")
+
+
+    print("      Menu      "
+          "\nselecione uma opção de 1 a 3:"
+          "\n1 - Fazer Login"
+          "\n2 - Resgistrar uma conta"
+          "\n3 - Sair")
+    resposta = int(input("Sua opção:"))
+
+    if resposta == 2:
+        registro()
+    elif resposta == 3:
+        break
+    if resposta == 1:
+        if login() == True:
+            print("      Menu do Sistema de Horários"
+                  "\n1 - Registrar Horário"
+                  "\n2 - Sair")
+            opcao = int(input("Sua opção: "))
+            if opcao == 2:
+                break
+            elif opcao == 1:
+
+                Segunda = int(input(""))
+
+
+            elif opcao == 2:
+                break
+
+            else:
+                print("Nenhum horário registrado ainda.\n")
+
